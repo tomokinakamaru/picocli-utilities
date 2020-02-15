8c830b61203aaa7e34bd017ae649f3211fa5457a
TomokiNakamaru <tomoki.nakamaru@gmail.com>
2020-02-15 15:05:53 +0900

    Update VersionProvider.java

--- src/main/java/com/github/tomokinakamaru/utility/picocli/VersionProvider.java
+++ src/main/java/com/github/tomokinakamaru/utility/picocli/VersionProvider.java
@@ -1,40 +1,12 @@
 package com.github.tomokinakamaru.utility.picocli;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URL;
-import java.util.Enumeration;
-import java.util.jar.Manifest;
-import picocli.CommandLine;
+import com.github.tomokinakamaru.manifestattributes.ManifestAttributes;
+import picocli.CommandLine.IVersionProvider;
 
-final class VersionProvider implements CommandLine.IVersionProvider {
+final class VersionProvider implements IVersionProvider {
 
   @Override
   public String[] getVersion() {
-    return new String[] {getManifest().getMainAttributes().getValue("Version")};
-  }
-
-  private Manifest getManifest() {
-    try {
-      return new Manifest(getURLStream());
-    } catch (IOException e) {
-      throw new RuntimeException(e);
-    }
-  }
-
-  private InputStream getURLStream() {
-    try {
-      return getResources().nextElement().openStream();
-    } catch (IOException e) {
-      throw new RuntimeException(e);
-    }
-  }
-
-  private Enumeration<URL> getResources() {
-    try {
-      return VersionProvider.class.getClassLoader().getResources("META-INF/MANIFEST.MF");
-    } catch (IOException e) {
-      throw new RuntimeException(e);
-    }
+    return new String[] {new ManifestAttributes(VersionProvider.class).get("Version")};
   }
 }
